0a42d6a665c07b4908fb8070401272d7fbb6dff5
Gorka <gorka@aquigorka.com>
2017-01-07 12:20:56 -0300

    Adds standardjs linter library. Fixes lint erros in theater/index.js only

--- src/theater/index.js
+++ src/theater/index.js
@@ -1,29 +1,29 @@
-"use strict";
+'use strict'
 
 //
-var React = require('react'),
-    Router = require('react-router'),
-    { Route, NotFoundRoute, DefaultRoute } = Router;
+const React = require('react')
+const Router = require('react-router')
+const { Route, NotFoundRoute, DefaultRoute } = Router
 
 //
-var App = require('./app.jsx'),
-    Home = require('./modules/home/home.jsx'),
-    ControlBar = require('./modules/control-bar/index.jsx'),
-    NotFound = require('./modules/not-found/not-found.jsx');
+const App = require('./app.jsx')
+const Home = require('./modules/home/home.jsx')
+const ControlBar = require('./modules/control-bar/index.jsx')
+const NotFound = require('./modules/not-found/not-found.jsx')
 
 //
-var routes = (
-    <Route handler={App} path="/">
-        <DefaultRoute handler={Home} />
-        <Route path="control-bar" handler={ControlBar} />
-        //
-        <NotFoundRoute handler={NotFound} />
-    </Route>
-);
+const routes = (
+  <Route handler={App} path='/'>
+    <DefaultRoute handler={Home} />
+    <Route path='control-bar' handler={ControlBar} />
+    //
+    <NotFoundRoute handler={NotFound} />
+  </Route>
+)
 
 //
 document.addEventListener('DOMContentLoaded', function () {
-    Router.run(routes, function (Handler, state) {
-        React.render(<Handler />, document.body);
-    });
-});
+  Router.run(routes, function (Handler, state) {
+    React.render(<Handler />, document.body)
+  })
+})
